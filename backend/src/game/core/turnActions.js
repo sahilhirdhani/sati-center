2e17e66d119d36dc229e1400b2f113fcc6323012
sahihirdhani <sahilhirdhani@gmail.com>
2026-02-03 01:08:08 +0530

    added bot auto move when there are less than 3 players

--- backend/src/game/core/turnActions.js
+++ backend/src/game/core/turnActions.js
@@ -1,3 +1,6 @@
+import { dispatchAction } from "../actions/dispatchAction.js";
+import { buildAction } from "../actions/gameAction.js";
+import { pickBotMove } from "../bot/botAction.js";
 import { remove } from "../state/gameState.js";
 import { getLegalMoves } from "../validation/rules.js";
 
@@ -5,12 +8,30 @@ function canPlayerTakeTurn (state, player) {
     if(!player.isActive) return false;
 
     const legalMoves = getLegalMoves(
-        player.hand, 
+        player.hand,
         state.table,
         state.config.layoutMode
     );
 
     return legalMoves.length > 0;
+}
+
+function canBotPlay (state, player) {
+    if(!player.isActive) return false
+
+    const legalMoves = getLegalMoves(
+        player.hand,
+        state.table,
+        state.config.layoutMode
+    )
+    // console.log("legal moves in turnActions: ", legalMoves)
+    if(legalMoves.length > 0){
+        const action = buildAction(player, pickBotMove(legalMoves))
+        dispatchAction(state, action)
+        // console.log("action of bot in turnAction: ", action)
+        return true
+    }
+    return false
 }
 
 export function getStartingPlayer (state) {
@@ -34,7 +55,12 @@ export function advanceTurn(state) {
         state.currentTurnIndex = (state.currentTurnIndex + 1) % total;
 
         const player = state.players[state.currentTurnIndex];
-        if(canPlayerTakeTurn(state, player)) {
+        // console.log(player.id)
+        if(player.isBot){
+            canBotPlay(state,player)
+            break
+        }
+        else if(canPlayerTakeTurn(state, player)) {
             return player;
         }
     }
@@ -47,12 +73,8 @@ export function getCurrentPlayer(state) {
 export function rollback(state, playedMoves){
     const lastMove = playedMoves.pop()
     state.currentTurnIndex = lastMove.lastPlayer.slice(-1)-1
-        
-    // console.log("state.currentTurnIndex: ",state.currentTurnIndex)
-    // console.log("lastMove.lastPlayer: ",lastMove.lastPlayer)
 
     const player = getCurrentPlayer(state)
-    // console.log(player.name, ": player.name")
     
     player.hand.push(lastMove.card)
     // ************************* using binary find the perfect position for inserting *************************
