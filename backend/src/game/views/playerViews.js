9f3f578d100cb68ed5eac4102b52abc0630c1bf3
sahihirdhani <sahilhirdhani@gmail.com>
2026-02-15 14:34:47 +0530

    cleaned

--- backend/src/game/views/playerViews.js
+++ backend/src/game/views/playerViews.js
@@ -1,37 +1,3 @@
-// import { getLegalMoves } from "../validation/rules"
-// import { getCurrentPlayer } from "../core/turnActions"
-
-// export function getPlayerView(state, playerId) {
-//     const player = state.players[playerId.slice(-1)]
-//     if(!player) throw new Error("Invalid Player")
-
-//     return {
-//         table: structuredClone(state.table),
-        
-//         you: {
-//             id: playerId,
-//             name: player.name,
-//             hand: structuredClone(player.hand),
-//             isActive: player.isActive
-//         },
-//         players: state.players.map(p => ({
-//             id: p.id,
-//             name: p.name,
-//             isActive: p.isActive,
-//             position: p.position
-//         })),
-//         currentTurnPlayerId: getCurrentPlayer(state).id,
-        
-//         legalMoves: getLegalMoves(
-//             player.hand,
-//             state.table,
-//             state.config.layoutMode
-//         ),
-//         finishedPlayers: structuredClone(state.finishedPlayers),
-//         winner: state.winner
-//     }
-// }
-
 import { getLegalMoves } from "../validation/rules.js";
 import { getCurrentPlayer } from "../core/turnActions.js";
 
