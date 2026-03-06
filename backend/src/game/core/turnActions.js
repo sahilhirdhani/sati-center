00b4ea16f1acce938de36ac6b657fa35993d86ce
sahihirdhani <sahilhirdhani@gmail.com>
2026-03-07 05:00:09 +0530

    fixed a bug, bug: if bot was initial player it wasnt taking a turn

--- backend/src/game/core/turnActions.js
+++ backend/src/game/core/turnActions.js
@@ -12,33 +12,27 @@ function canPlayerTakeTurn (state, player) {
         state.table,
         state.config.layoutMode
     );
-
     return legalMoves.length > 0;
 }
 
 function canBotPlay (state, player) {
     if(!player.isActive) return false
-
     const legalMoves = getLegalMoves(
         player.hand,
         state.table,
         state.config.layoutMode
     )
-    // console.log("legal moves in turnActions: ", legalMoves)
     if(legalMoves.length > 0){
         const action = buildAction(player, pickBotMove(legalMoves))
         dispatchAction(state, action)
-        // console.log("action of bot in turnAction: ", action)
         return true
     }
     return false
 }
 
 export function getStartingPlayer (state) {
-    // console.log(state)
     for (let i=0; i<state.players.length; i++) {
         const player = state.players[i];
-        // console.log(player)
         if(canPlayerTakeTurn(state, player)) {
             if(player.id.startsWith('bot')){
                 const legalMoves = getLegalMoves(
@@ -46,8 +40,10 @@ export function getStartingPlayer (state) {
                     state.table,
                     state.config.layoutMode
                 )
+                state.currentTurnIndex = i;
                 const action = buildAction(player, pickBotMove(legalMoves))
                 dispatchAction(state, action)
+                return player;
             }
             state.currentTurnIndex = i;
             return player;
@@ -64,7 +60,6 @@ export function advanceTurn(state) {
         state.currentTurnIndex = (state.currentTurnIndex + 1) % total;
 
         const player = state.players[state.currentTurnIndex];
-        // console.log(player.id)
         if(player.isBot){
             if(!canBotPlay(state,player)) continue
             break
--- backend/src/game/state/gameSetup.js
+++ backend/src/game/state/gameSetup.js
@@ -8,6 +8,68 @@ import { createGameState } from "./gameState.js";
 
 const MIN_PLAYERS = 3;
 
+function batoPatte(deck, players) {
+    
+    players[0].hand=[
+        {suit: 'clubs', value: 1, deckId: 0, id: 'clubs-1-0'},
+        {suit: 'clubs', value: 2, deckId: 0, id: 'clubs-2-0'},
+        {suit: 'clubs', value: 3, deckId: 0, id: 'clubs-3-0'},
+        {suit: 'clubs', value: 4, deckId: 0, id: 'clubs-4-0'},
+        {suit: 'clubs', value: 5, deckId: 0, id: 'clubs-5-0'},
+        {suit: 'clubs', value: 6, deckId: 0, id: 'clubs-6-0'},
+        {suit: 'clubs', value: 8, deckId: 0, id: 'clubs-8-0'},
+        {suit: 'clubs', value: 9, deckId: 0, id: 'clubs-9-0'},
+        {suit: 'clubs', value: 10, deckId: 0, id: 'clubs-10-0'},
+        {suit: 'clubs', value: 11, deckId: 0, id: 'clubs-11-0'},
+        {suit: 'clubs', value: 12, deckId: 0, id: 'clubs-12-0'},
+        {suit: 'clubs', value: 13, deckId: 0, id: 'clubs-13-0'},
+        {suit: 'diamonds', value: 1, deckId: 0, id: 'diamonds-1-0'},
+        {suit: 'diamonds', value: 2, deckId: 0, id: 'diamonds-2-0'},
+        {suit: 'diamonds', value: 3, deckId: 0, id: 'diamonds-3-0'},
+        {suit: 'diamonds', value: 4, deckId: 0, id: 'diamonds-4-0'},
+        {suit: 'diamonds', value: 5, deckId: 0, id: 'diamonds-5-0'}
+    ]
+    players[1].hand = [
+        {suit:'clubs',value:7,deckId:0,id:'clubs-7-0'},
+        {suit:'diamonds',value:6,deckId:0,id:'diamonds-6-0'},
+        {suit:'diamonds',value:7,deckId:0,id:'diamonds-7-0'},
+        {suit:'diamonds',value:8,deckId:0,id:'diamonds-8-0'},
+        {suit:'diamonds',value:9,deckId:0,id:'diamonds-9-0'},
+        {suit:'diamonds',value:10,deckId:0,id:'diamonds-10-0'},
+        {suit:'diamonds',value:11,deckId:0,id:'diamonds-11-0'},
+        {suit:'diamonds',value:12,deckId:0,id:'diamonds-12-0'},
+        {suit:'diamonds',value:13,deckId:0,id:'diamonds-13-0'},
+        {suit:'hearts',value:1,deckId:0,id:'hearts-1-0'},
+        {suit:'hearts',value:2,deckId:0,id:'hearts-2-0'},
+        {suit:'hearts',value:3,deckId:0,id:'hearts-3-0'},
+        {suit:'hearts',value:4,deckId:0,id:'hearts-4-0'},
+        {suit:'hearts',value:5,deckId:0,id:'hearts-5-0'},
+        {suit:'hearts',value:6,deckId:0,id:'hearts-6-0'},
+        {suit:'hearts',value:7,deckId:0,id:'hearts-7-0'},
+        {suit:'hearts',value:8,deckId:0,id:'hearts-8-0'}
+    ]
+    players[2].hand = [
+        {suit:'hearts',value:9,deckId:0,id:'hearts-9-0'},
+        {suit:'hearts',value:10,deckId:0,id:'hearts-10-0'},
+        {suit:'hearts',value:11,deckId:0,id:'hearts-11-0'},
+        {suit:'hearts',value:12,deckId:0,id:'hearts-12-0'},
+        {suit:'hearts',value:13,deckId:0,id:'hearts-13-0'},
+        {suit:'spades',value:1,deckId:0,id:'spades-1-0'},
+        {suit:'spades',value:2,deckId:0,id:'spades-2-0'},
+        {suit:'spades',value:3,deckId:0,id:'spades-3-0'},
+        {suit:'spades',value:4,deckId:0,id:'spades-4-0'},
+        {suit:'spades',value:5,deckId:0,id:'spades-5-0'},
+        {suit:'spades',value:6,deckId:0,id:'spades-6-0'},
+        {suit:'spades',value:7,deckId:0,id:'spades-7-0'},
+        {suit:'spades',value:8,deckId:0,id:'spades-8-0'},
+        {suit:'spades',value:9,deckId:0,id:'spades-9-0'},
+        {suit:'spades',value:10,deckId:0,id:'spades-10-0'},
+        {suit:'spades',value:11,deckId:0,id:'spades-11-0'},
+        {suit:'spades',value:12,deckId:0,id:'spades-12-0'},
+        {suit:'spades',value:13,deckId:0,id:'spades-13-0'}
+    ]
+}
+
 function buildPlayer(players) {
     for(let i=0; i < players.length; i++){
         players[i] = new Player(players[i].id, players[i].name)
@@ -72,6 +134,9 @@ export function setupGame(players, layoutMode, cheatsEnabled) {
     placeInitialSeven(state.table, initialSeven, config.layoutMode);
 
     dealCards(deck, state.players);
+
+    // for testing purposes, giving specific cards to players to test the game flow and bot moves
+    // batoPatte(deck, state.players) 
 
     state.started = true;
 
