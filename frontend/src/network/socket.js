a0e755ce7414ef5cd5251cd990d0610431a8b8be
sahihirdhani <sahilhirdhani@gmail.com>
2026-07-23 19:28:21 +0530

    minor changes

--- frontend/src/network/socket.js
+++ frontend/src/network/socket.js
@@ -3,9 +3,9 @@ import { io } from "socket.io-client";
 let socket = null;
 
 const BACKEND_SOCKET_URL =
-    import.meta.env.VITE_BACKEND_SOCKET_URL || "https://satti-center.onrender.com";
+    import.meta.env.VITE_BACKEND_SOCKET_URL;
 const LOCAL_SOCKET_URL =
-    import.meta.env.VITE_LOCAL_SOCKET_URL || "http://localhost:5000";
+    import.meta.env.VITE_LOCAL_SOCKET_URL;
 
 const getSocketUrl = () => {
     if (import.meta.env.VITE_SOCKET_URL) {
